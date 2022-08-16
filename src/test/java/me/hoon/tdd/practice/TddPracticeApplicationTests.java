1c869fb6cf37474f81f8360724619e77e0de1dca
hoon <dltjdgnsok1@naver.com>
2022-08-16 23:54:26 +0900

    Initial commit

---
+++ src/test/java/me/hoon/tdd/practice/TddPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hoon.tdd.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
