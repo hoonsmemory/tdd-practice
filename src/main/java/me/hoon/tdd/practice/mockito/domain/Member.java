efc3df787b6e17cf11053f3aa728a7cfc95a81b5
hoon <dltjdgnsok1@naver.com>
2022-08-20 14:24:44 +0900

    필드 추가

--- src/main/java/me/hoon/tdd/practice/mockito/domain/Member.java
+++ src/main/java/me/hoon/tdd/practice/mockito/domain/Member.java
@@ -2,4 +2,31 @@ package me.hoon.tdd.practice.mockito.domain;
 
 
 public class Member {
+    private long id;
+    private String email;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/me/hoon/tdd/practice/mockito/domain/Study.java
+++ src/main/java/me/hoon/tdd/practice/mockito/domain/Study.java
@@ -6,7 +6,16 @@ public class Study {
 
     private Member owner;
 
+    private int limit;
+    private String name;
+
+    public Study(int limit, String name) {
+        this.limit = limit;
+        this.name = name;
+    }
+
     public void setOwner(Member owner) {
         this.owner = owner;
     }
+
 }
