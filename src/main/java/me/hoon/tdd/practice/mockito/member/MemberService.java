c0ea50474c9f3911bd46e553a45dedc3b6de73ab
hoon <dltjdgnsok1@naver.com>
2022-08-20 16:07:52 +0900

    테스트할 메서드 추가

--- src/main/java/me/hoon/tdd/practice/mockito/member/MemberService.java
+++ src/main/java/me/hoon/tdd/practice/mockito/member/MemberService.java
@@ -1,10 +1,15 @@
 package me.hoon.tdd.practice.mockito.member;
 
 import me.hoon.tdd.practice.mockito.domain.Member;
+import me.hoon.tdd.practice.mockito.domain.Study;
 
 import java.util.Optional;
 
 public interface MemberService {
 
     Optional<Member> findById(Long memberId) throws MemberNotFoundException;
+
+    void notify(Study study);
+
+    void notify(Member member);
 }
