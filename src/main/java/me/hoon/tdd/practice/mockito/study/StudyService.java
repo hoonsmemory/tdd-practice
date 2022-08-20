ec8f52041d656211a9bd09ed45c820d09e5cbb25
hoon <dltjdgnsok1@naver.com>
2022-08-20 16:08:16 +0900

    테스트 메서드 수정

--- src/main/java/me/hoon/tdd/practice/mockito/study/StudyService.java
+++ src/main/java/me/hoon/tdd/practice/mockito/study/StudyService.java
@@ -22,8 +22,12 @@ public class StudyService {
 
     public Study createNewStudy(Long memberId, Study study) {
         Optional<Member> member = memberService.findById(memberId);
-
         study.setOwner(member.orElseThrow(()-> new IllegalArgumentException("member doesn't exist for id : " + memberId)));
-        return studyRepository.save(study);
+
+        Study newStudy = studyRepository.save(study);
+        memberService.notify(newStudy);
+        memberService.notify(member.get());
+
+        return newStudy;
     }
 }
